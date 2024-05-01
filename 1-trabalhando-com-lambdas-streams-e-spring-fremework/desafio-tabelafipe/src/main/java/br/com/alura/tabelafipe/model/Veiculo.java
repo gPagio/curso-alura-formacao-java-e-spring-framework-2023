2f1fca9c56cb279fd25dfd7487d1a1f24fa4b3cf
gPagio <guilhermepagio.contato@gmail.com>
2024-05-01 19:47:30 -0300

    chore: adiciona .gitignore

--- 1-trabalhando-com-lambdas-streams-e-spring-fremework/desafio-tabelafipe/src/main/java/br/com/alura/tabelafipe/model/Veiculo.java
+++ 1-trabalhando-com-lambdas-streams-e-spring-fremework/desafio-tabelafipe/src/main/java/br/com/alura/tabelafipe/model/Veiculo.java
@@ -1,26 +1,20 @@
 package br.com.alura.tabelafipe.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-public record Veiculo(@JsonAlias("TipoVeiculo") Integer tipoVeiculo,
-                      @JsonAlias("Valor") String valor,
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Veiculo(@JsonAlias("Valor") String valor,
                       @JsonAlias("Marca") String marca,
                       @JsonAlias("Modelo") String modelo,
                       @JsonAlias("AnoModelo") Integer anoModelo,
-                      @JsonAlias("Combustivel") String combustivel,
-                      @JsonAlias("CodigoFipe") String codigoFipe,
-                      @JsonAlias("MesReferencia") String mesReferencia,
-                      @JsonAlias("SiglaCombustivel") String siglaCombustivel) {
+                      @JsonAlias("Combustivel") String combustivel) {
     @Override
     public String toString() {
-        return "Tipo Veículo: " + tipoVeiculo + "\n" +
-                "Valor: " + valor + "\n" +
+        return "Valor: " + valor + "\n" +
                 "Marca: " + marca + "\n" +
                 "Modelo: " + modelo + "\n" +
                 "Ano Modelo: " + anoModelo + "\n" +
-                "Combustível: " + combustivel + "\n" +
-                "Código Fipe: " + codigoFipe + "\n" +
-                "Mês Referência: " + mesReferencia + "\n" +
-                "Sigla Combustível: " + siglaCombustivel;
+                "Combustível: " + combustivel + "\n";
     }
 }
