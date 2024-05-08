f8b9b9f0293c687f72fc91ccbcdd3ebd997de9a7
Guilherme Pagio <116508581+gPagio@users.noreply.github.com>
2024-05-08 17:42:46 -0300

    feat: modelagem entidades desafio

--- 2-persistencia-de-dados-e-consultas-com-spring-data-jpa/desafio/src/main/java/br/com/alura/screensound/model/Artista.java
+++ 2-persistencia-de-dados-e-consultas-com-spring-data-jpa/desafio/src/main/java/br/com/alura/screensound/model/Artista.java
@@ -1,15 +1,62 @@
 package br.com.alura.screensound.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "artistas")
 public class Artista {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String nome;
+    @Enumerated(EnumType.STRING)
+    private TipoArtista tipo;
+    @OneToMany(mappedBy = "artista")
+    private List<Musica> musicas = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoArtista getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoArtista tipo) {
+        this.tipo = tipo;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", tipo=" + tipo +
+                ", musicas=" + musicas +
+                '}';
+    }
 }
