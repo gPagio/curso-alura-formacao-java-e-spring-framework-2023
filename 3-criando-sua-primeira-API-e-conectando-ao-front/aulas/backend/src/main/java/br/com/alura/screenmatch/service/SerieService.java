b9cb9d59f2e7778c504a1a5b8828aa6d376045a8
gPagio <guilhermepagio.contato@gmail.com>
2024-05-11 13:27:27 -0300

    feat: aula 3
    Commit subindo tudo o que foi feito durante a aula 3

--- 3-criando-sua-primeira-API-e-conectando-ao-front/aulas/backend/src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ 3-criando-sua-primeira-API-e-conectando-ao-front/aulas/backend/src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,11 +1,13 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,7 +17,16 @@ public class SerieService {
     private SerieRepository respositorio;
 
     public List<SerieDTO> obterTodasAsSeries (){
-        return respositorio.findAll().stream()
+        return listSerieToListSerieDTO(respositorio.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series(){
+        return listSerieToListSerieDTO(respositorio.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    private List<SerieDTO> listSerieToListSerieDTO(List<Serie> listaSerie){
+        return listaSerie.stream()
                 .map(serie -> new SerieDTO(serie.getId()
                         , serie.getTitulo()
                         , serie.getTotalTemporadas()
@@ -24,5 +35,19 @@ public class SerieService {
                         , serie.getAtores()
                         , serie.getPoster()
                         , serie.getSinopse())).collect(Collectors.toList());
+    }
+
+    public List<SerieDTO> obterLancamento() {
+        return listSerieToListSerieDTO(respositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    }
+
+    public SerieDTO obterSeriePorId(Long id) {
+        Optional<Serie> serie = respositorio.findById(id);
+
+        if (serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
     }
 }
