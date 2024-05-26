8f1b4e292998b8652bcc956ec667e8eed7e40c6a
gPagio <guilhermepagio.contato@gmail.com>
2024-05-26 09:59:49 -0300

    refactor: renomear  3-criando-sua-primeira-API-e-conectando-ao-front para  3-criando-sua-primeira-API_TEMP-e-conectando-ao-front

---
+++ 3-criando-sua-primeira-API_TEMP-e-conectando-ao-front/aulas/backend/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -0,0 +1,60 @@
+package br.com.alura.screenmatch.controller;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.dto.EpisodioDTO;
+import br.com.alura.screenmatch.service.SerieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/series")
+public class SerieController {
+
+    @Autowired
+    private SerieService servico;
+
+    @GetMapping
+    public List<SerieDTO> obterSeries(){
+        return servico.obterTodasAsSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series(){
+        return servico.obterTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> obterLancamentos(){
+        return servico.obterLancamento();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterSeriePorId(@PathVariable Long id){
+        return servico.obterSeriePorId(id);
+    }
+
+    @GetMapping("/{id}/temporadas/todas")
+    public List<EpisodioDTO> obterTodasAsTemporadas(@PathVariable Long id){
+        return servico.obterTodasAsTemporadas(id);
+    }
+
+    @GetMapping("/{id}/temporadas/{numero}")
+    public List<EpisodioDTO> obterTemporadaEspecifica(@PathVariable Long id, @PathVariable Long numero){
+        return servico.obterTemporadaEspecifica(id, numero);
+    }
+
+    @GetMapping("/categoria/{nomeGenero}")
+    public List<SerieDTO> obterSeriesPorGenero(@PathVariable String nomeGenero){
+        return servico.obterSeriesPorGenero(nomeGenero);
+    }
+
+    @GetMapping("{id}/temporadas/top")
+    public List<EpisodioDTO> obterTop5EpisodiosPorSerie(@PathVariable("id") Long idSerie){
+        return servico.obterTop5EpisodiosPorSerie(idSerie);
+    }
+}
