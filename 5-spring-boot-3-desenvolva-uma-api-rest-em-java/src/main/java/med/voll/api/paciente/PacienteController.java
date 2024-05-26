e2933d420a32e037bd381d586d2409b3ab556172
gPagio <guilhermepagio.contato@gmail.com>
2024-05-26 10:17:48 -0300

    feat: aula 2
    Commit com tudo o que foi feito durante a aula 2

--- 5-spring-boot-3-desenvolva-uma-api-rest-em-java/src/main/java/med/voll/api/paciente/PacienteController.java
+++ 5-spring-boot-3-desenvolva-uma-api-rest-em-java/src/main/java/med/voll/api/paciente/PacienteController.java
@@ -1,4 +1,4 @@
-package med.voll.api.controller;
+package med.voll.api.paciente;
 
 import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("medicos")
-public class MedicoController {
+@RequestMapping("pacientes")
+public class PacienteController {
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+    public void cadastrar(@RequestBody DadosCadastroPaciente dados){
         System.out.println(dados);
     }
-
 }
