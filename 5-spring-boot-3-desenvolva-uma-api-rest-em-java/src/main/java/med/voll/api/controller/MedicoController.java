cd18f5035797509ff3b55f9d0f3a943847a77460
gPagio <guilhermepagio.contato@gmail.com>
2024-05-31 11:13:43 -0300

    feat: aula 4
    Commit com tudo o que foi feito durante a aula 4

--- 5-spring-boot-3-desenvolva-uma-api-rest-em-java/src/main/java/med/voll/api/controller/MedicoController.java
+++ 5-spring-boot-3-desenvolva-uma-api-rest-em-java/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,14 +2,18 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Comparator;
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -24,4 +28,8 @@ public class MedicoController {
         repository.save(new Medico(dadosCadastroMedico));
     }
 
+    @GetMapping
+    public Page<DadosListagemMedicos> listar(@PageableDefault(size = 10, sort = "nome") Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedicos::new);
+    }
 }
