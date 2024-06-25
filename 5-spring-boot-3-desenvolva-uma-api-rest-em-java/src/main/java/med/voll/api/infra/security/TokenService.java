71a3db07549a7837813901020768a9dab7b7d171
gPagio <guilhermepagio.contato@gmail.com>
2024-06-25 19:12:48 -0300

    feat: aula 5
    Commit com tudo o que foi feito durante a aula 5
    
    A mensagem dos commits está igual aos commits anteriores, porém a mesma se refere a um novo curso. Isso aconteceu pois o novo curso em questão está usando o mesmo projeto do curso anteior

--- 5-spring-boot-3-desenvolva-uma-api-rest-em-java/src/main/java/med/voll/api/infra/security/TokenService.java
+++ 5-spring-boot-3-desenvolva-uma-api-rest-em-java/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -17,17 +17,32 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
+    private static final String ISSUER = "API Voll.med";
+
     public String gerarToken(Usuario usuario) {
         try {
             var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
-                    .withIssuer("API Voll.med")
+                    .withIssuer(ISSUER)
                     .withSubject(usuario.getUsername())
                     .withClaim("id", usuario.getId())
                     .withExpiresAt(dataExpiracao())
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer(ISSUER)
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!", exception);
         }
     }
 
